3a6e9ccc71fdcdf3fdea38275130a97d366cde0c
FranScarvaci <83133797+FranScarvaci@users.noreply.github.com>
2021-04-30 00:37:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("testing")
+import localQuotes from './quotes.js';
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote fron apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
+  console.log(quote);
+}
+// On Load
+// getQuotes();
+newQuote();
